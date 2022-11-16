8b85449df5a51c01a2b117732c96715b00f41438
Mysterium-sch <LambLily90@gmail.com>
2022-11-16 14:51:17 -0500

    one thread works

--- repo/Lab3_reconfigure.cpp
+++ repo/Lab3_reconfigure.cpp
@@ -6,11 +6,14 @@
 #include <thread> 
 #include <mutex>
 #include <queue>
+#include <stdlib.h>
 
 #include "Binary_st.cpp"
 
 using namespace std;
 
+vector<string> words;
+int words_in = 0;
 const int size = pow(2,22);
 int total = 0;
 BinaryTree map;
@@ -26,7 +29,7 @@ pthread_mutex_t mutexer = PTHREAD_MUTEX_INITIALIZER;
 
 // Methods
 void* insert(void*);
-void inserter(int, string);
+void inserter(string);
 
 void* deleter(void*);
 void deleter_helper(int);
@@ -72,18 +75,19 @@ int main(int argc, char **argv) {
 
         // Proccess Info
     while(!actionQueue.empty()) {
-    
-        while(threads.size() < threadcount && !actionQueue.empty()) {
+
+        index = 0;
+        while(index<threadcount && !actionQueue.empty()) {
         string temp;
         value = "";
         string nextAction = actionQueue.front();
-        stringstream words(nextAction);
+        stringstream words_A(nextAction);
 
-        words >> type;
-        words >> key;
+        words_A >> type;
+        words_A >> key;
         //cout << key << "\n";
         int keyRand = stoi(key);
-        while (words >> temp)
+        while (words_A >> temp)
             value += temp + " ";
         
         // Proccess action
@@ -91,8 +95,10 @@ int main(int argc, char **argv) {
         if(type.compare("I") == 0) {
             //insert
             if(total<size) {
-            string holder = key + " " + value;
-            threads.emplace( pthread_create(&ptid[index], NULL, &insert, (void*) &holder));
+                string holder = to_string(keyRand) + " " + value;
+                words.push_back(holder);
+            threads.push( pthread_create(&ptid[index], NULL, &insert, (void*) &words_in));
+            words_in += 1;
             } else {
                 string out = "Max size of " + to_string(size) + " reached";
                 output.push(out);
@@ -102,26 +108,21 @@ int main(int argc, char **argv) {
             //delete
             int *h = &keyRand;
             threads.push( pthread_create(&ptid[index], NULL, &deleter, (void*) h));
-            std::this_thread::sleep_for (std::chrono::milliseconds(10));
         } else if (type.compare("L") == 0) {
             //lookup
             int *h = &keyRand;
             threads.push( pthread_create(&ptid[index], NULL, &lookup, (void*) h));
-            std::this_thread::sleep_for (std::chrono::milliseconds(10));
         } else {
             cout << "Invalid Format\n";
         }
         actionQueue.pop();
-        index = (index+1)%threadcount;
+        index++;
         }
-        // Make sure threads stay under 4
-        
-        
-            pthread_cond_signal(&cond1);
-            pthread_join(threads.front(), NULL);
-            threads.pop();
-    }
 
+        for(int i = 0; i<threads.size(); i++) {
+            pthread_join(ptid[i], NULL);
+        }
+    }
 
     //Output to file
     ofstream outFile;
@@ -133,16 +134,22 @@ int main(int argc, char **argv) {
     }
 
     cout << "File Execution Completed\n";
-
 }
 
 void* insert(void *v) {
     pthread_mutex_lock(&mutexer);
-    pthread_cond_wait(&cond1, &mutexer);
+    //pthread_cond_wait(&cond1, &mutexer);
+    string keyAndValue = words.at((*((int *)v)-1));
+    threads.pop();
+    
+    inserter(keyAndValue);
 
-    string *keyAndValue = (string *) v;
-   
-    stringstream ss(*keyAndValue);
+    pthread_mutex_unlock(&mutexer);
+    return 0;
+
+}
+void inserter(string keyAndValue) {
+    stringstream ss(keyAndValue);
     string kv, value, hold;
     ss >> kv;
     while (ss>>hold) {
@@ -150,34 +157,27 @@ void* insert(void *v) {
     }
 
     int key = stoi(kv);
-    inserter(key, value);
     
-    pthread_mutex_unlock(&mutexer);
-
-    return 0;
-
-}
-void inserter(int key, string textt) {
-    if(map.insert(key, textt)) {
-        cout << textt << " : " << key <<  " OK\n";
+    if(map.insert(key, value)) {
+        cout << value << " : " << key <<  " OK\n";
         total++;
         output.push("OK");
     } else {
-        cout << textt << " : " << key <<  " FAIL\n";
+        cout << value << " : " << key <<  " FAIL\n";
         output.push("FAIL");
     }
 }
 
 void* deleter(void *v) {
-    // wait on insert/mutex
-    // wait on lookup/mutex
 
-    //std::lock_guard<std::mutex> guard(mutexer);
+    pthread_mutex_lock(&mutexer);
 
     int *key = (int *) v;
     deleter_helper(*key);
 
     threads.pop();
+
+    pthread_mutex_unlock(&mutexer);
 
     return 0;
     
@@ -192,14 +192,14 @@ void deleter_helper(int key) {
 }
 
 void* lookup (void *v) {
-    // wait on insert/mutex
-    // wait on delete/mutex
+    pthread_mutex_lock(&mutexer);
 
-    //std::lock_guard<std::mutex> guard(mutexer);
     int *key = (int *) v;
     lookup_helper(*key);
 
     threads.pop();
+
+    pthread_mutex_unlock(&mutexer);
     
     return 0;
 }
