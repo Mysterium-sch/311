fec5c010ee6fe0ee8e8eda163bd130f82edbc1de
Mysterium-sch <LambLily90@gmail.com>
2022-11-16 21:15:19 -0500

    this bitch finally works

--- repo/Lab3.cpp
+++ repo/Lab3.cpp
@@ -3,16 +3,13 @@
 #include<cmath>
 #include <fstream>
 #include <sstream>
-#include <thread> 
-#include <mutex>
 #include <queue>
-#include <stdlib.h>
+#include <pthread.h>
 
 #include "Binary_st.cpp"
 
 using namespace std;
 
-vector<string> words;
 int words_in = 0;
 const int size = pow(2,22);
 int total = 0;
@@ -20,24 +17,17 @@ BinaryTree map;
 
 // Indivudal operation queue
 queue<string> actionQueue;
-queue<pthread_t> threads;
 queue<string> output;
 
 // Pthread things
-pthread_cond_t cond1 = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mutexer = PTHREAD_MUTEX_INITIALIZER;
 
 // Methods
 void* letEmRun(void *);
 
-void* insert(void*);
 void inserter(string);
-
-void* deleter(void*);
-void deleter_helper(int);
-
-void* lookup(void*);
-void lookup_helper(int);
+void deleter(int);
+void lookup(int);
 
 int main(int argc, char **argv) { 
 
@@ -62,7 +52,7 @@ int main(int argc, char **argv) {
     }
 
     pthread_t ptid[threadcount];
-    int index = 0;
+    int index;
 
     //Pull all actions from input file
     while(input_file.good()) {
@@ -77,18 +67,41 @@ int main(int argc, char **argv) {
 
         // Proccess Info
     while(!actionQueue.empty()) {
-
-        // Queue up threads
         index = 0;
-        while(index<threadcount && !actionQueue.empty()) {
-        string temp;
-        value = "";
+        // create the threads
+        while(index < threadcount && !actionQueue.empty()) {
+            pthread_create(&ptid[index], NULL, &letEmRun, (void *) &index);
+            index++;
+        }
+        // process the threads
+        index = 0;
+        while(index < threadcount && !actionQueue.empty()) {
+            pthread_join(ptid[index], NULL);
+            pthread_detach(ptid[index]);
+            index++;
+        }
+    }
+
+    //Output to file
+    ofstream outFile;
+    outFile.open(out_filename);
+    while(!output.empty()) {
+        outFile << output.front() << "\n";
+        output.pop();
+    }
+
+    cout << "File Execution Completed\n";
+}
+
+void* letEmRun(void *v) {
+    pthread_mutex_lock(&mutexer);
         string nextAction = actionQueue.front();
+        string temp, type, key;
+        string value = "";
         stringstream words_A(nextAction);
 
         words_A >> type;
         words_A >> key;
-        //cout << key << "\n";
         int keyRand = stoi(key);
         while (words_A >> temp)
             value += temp + " ";
@@ -99,9 +112,7 @@ int main(int argc, char **argv) {
             //insert
             if(total<size) {
                 string holder = to_string(keyRand) + " " + value;
-                words.push_back(holder);
-            threads.push( pthread_create(&ptid[index], NULL, &insert, (void*) &words_in));
-            words_in += 1;
+                inserter(holder);
             } else {
                 string out = "Max size of " + to_string(size) + " reached";
                 output.push(out);
@@ -109,59 +120,19 @@ int main(int argc, char **argv) {
 
         } else if (type.compare("D") == 0) {
             //delete
-            int *h = &keyRand;
-            threads.push( pthread_create(&ptid[index], NULL, &deleter, (void*) h));
+            deleter(keyRand);
         } else if (type.compare("L") == 0) {
             //lookup
-            int *h = &keyRand;
-            threads.push( pthread_create(&ptid[index], NULL, &lookup, (void*) h));
+            lookup(keyRand);
         } else {
-            cout << "Invalid Format\n";
+            output.push("Invalid Format");
         }
-        actionQueue.pop();
-        index++;
-        }
-
-        // Proccess the queued threads
-        for(int i = 0; i<threads.size(); i++) {
-            pthread_t val;
-            pthread_create(&val, NULL, &letEmRun, (void *) val);
-            pthread_join(val, NULL);
-        }
-    }
-
-    //Output to file
-    ofstream outFile;
-    outFile.open(out_filename);
-    while(!output.empty()) {
-        outFile << output.front() << "\n";
-        output.pop();
-
-    }
-
-    cout << "File Execution Completed\n";
-}
-
-void* letEmRun(void *v) {
-    pthread_mutex_lock(&mutexer);
-    pthread_cond_signal(&cond1);
+    actionQueue.pop();
     pthread_mutex_unlock(&mutexer);
     pthread_exit(NULL);
 }
 
-void* insert(void *v) {
-    pthread_mutex_lock(&mutexer);
-    pthread_cond_wait(&cond1, &mutexer);
-    string keyAndValue = words.at((*((int *)v)-1));
-    threads.pop();
-    
-    inserter(keyAndValue);
-    
-    pthread_mutex_unlock(&mutexer);
-    
-    pthread_exit(NULL);
 
-}
 void inserter(string keyAndValue) {
     stringstream ss(keyAndValue);
     string kv, value, hold;
@@ -173,30 +144,13 @@ void inserter(string keyAndValue) {
     int key = stoi(kv);
     
     if(map.insert(key, value)) {
-        cout << value << " : " << key <<  " OK\n";
         total++;
         output.push("OK");
     } else {
-        cout << value << " : " << key <<  " FAIL\n";
         output.push("FAIL");
     }
 }
-
-void* deleter(void *v) {
-
-    pthread_mutex_lock(&mutexer);
-
-    int *key = (int *) v;
-    deleter_helper(*key);
-
-    threads.pop();
-
-    pthread_mutex_unlock(&mutexer);
-
-    return 0;
-    
-}
-void deleter_helper(int key) {
+void deleter(int key) {
     if(map.remove_key(key)) {
          output.push("OK");
         total--;
@@ -204,20 +158,7 @@ void deleter_helper(int key) {
         output.push("FAIL");
     }
 }
-
-void* lookup (void *v) {
-    pthread_mutex_lock(&mutexer);
-
-    int *key = (int *) v;
-    lookup_helper(*key);
-
-    threads.pop();
-
-    pthread_mutex_unlock(&mutexer);
-    
-    return 0;
-}
-void lookup_helper(int key) {
+void lookup(int key) {
     string found = map.Keysearcher(key);
         if(found.compare("nulll") != 0) {
             output.push(found);
